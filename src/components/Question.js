254b71c881b223df93533964088d310171595dff
Maria Reedstrom <maria.reedstrom@gmail.com>
2022-02-02 16:29:15 -0600

    commit tree 51fb2c3ac8380bd04eed118be1a1fa6a03d3a4c1

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    if (timeRemaining === -1) {
+      setTimeRemaining(10);
+      onAnswered(false);
+      return;
+    } else {
+      const timer = setTimeout(() => {
+        setTimeRemaining(timeRemaining - 1);
+      }, 1000);
+      return function cleanup() {
+        clearTimeout(timer);
+      };
+    }
+  }, [timeRemaining, onAnswered]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
